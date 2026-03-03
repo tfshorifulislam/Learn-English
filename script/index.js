7159ed76ae75e168b20ba3b13f5d997430c560b6
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-03 08:33:40 +0600

    Add vocabulary in card dynamically

--- script/index.js
+++ script/index.js
@@ -11,17 +11,30 @@ const wordContainer = (id)=>{
     .then(res => res.json())
     .then(data => displayWordContainer(data.data))
 }
-// word container display;
+
 const displayWordContainer = (words)=>{
     const levelWord = document.getElementById('word-container')
-    // levelWord.innerHTML = ''
+    levelWord.innerHTML = ''
     for(let word of words){
         const card = document.createElement('div')
         card.innerHTML=`
-        
+        <div class="bg-white py-10 px-5 rounded-sm text-center space-y-4">
+                <h2 class="font-bold text-3xl">${word.word}</h2>
+                <p class="font-medium text-lg">Meaning /Pronounciation</p>
+                <p class="font-semibold text-3xl font-bangla">"${word.meaning}/ ${word.pronunciation}"</p>
+
+                <div class="flex justify-between items-center">
+                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
+                        <i class="fa-solid fa-circle-info"></i>
+                    </button>
+                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
+                       <i class="fa-solid fa-volume-high"></i>
+                    </button>
+                </div>
+        </div>
         
         `
-        levelWord.appendChild('card')
+        levelWord.appendChild(card)
     }
 }
 // Data load & display Lesson button section;
